051ba38eca92cbb4895636c067fe7e8dc9cc51e7
rafael.soares <rafaeldinizsoaresreal@gmail.com>
2022-01-06 17:43:56 -0300

    06/01

--- app/src/main/java/com/example/replicaivendasx/MainActivity.java
+++ app/src/main/java/com/example/replicaivendasx/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_telaexistente);
+        setContentView(R.layout.activity_main2);
     }
 }
